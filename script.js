90c0777fd112e8ea12068de9099ebb070709114d
Munia Sircar <moonish.nutcase1991@gmail.com>
2025-08-31 10:19:05 -0500

    Added AI API with prompt and context

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings:
-      "Knowing what it feels like to be in pain, is exactly why we try to kind to others. - Jiraiya, Naruto",
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let inputAnime = document.querySelector("#anime-input");
+  let apiKey = "28d0bff0ffaa52b09e33da6etode543b";
+  let prompt = `Generate a quote from the anime called ${inputAnime.value}.`;
+  let context =
+    "You are an anime fan. Provide a quote from the anime that is requested in basic HTML. Do not mention 'basic HTML', only show the quote and the person who said it.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let getQuote = document.querySelector("#get-quote");
